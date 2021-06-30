15ed769d05945a12f955875b09a8fb1c6a373d21
mansoorafzal <mansoorafzal@gmail.com>
2021-06-30 12:55:45 +0400

    remove unused variable

--- Shopping/Shopping.Client/Controllers/HomeController.cs
+++ Shopping/Shopping.Client/Controllers/HomeController.cs
@@ -8,11 +8,8 @@ namespace Shopping.Client.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
